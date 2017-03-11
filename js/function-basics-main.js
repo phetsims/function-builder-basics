b4c272c16ac982a94745f114731cdd99ca6967c8
Chris Malley <cmalley@pixelzoom.com>
2017-03-11 10:36:32 -0700

    set up basic structure of function-basics

---
+++ js/function-basics-main.js
@@ -0,0 +1,49 @@
+// Copyright 2015-2016, University of Colorado Boulder
+
+/**
+ * Main entry point for the 'Function Basics' sim.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var FBQueryParameters = require( 'FUNCTION_BUILDER/common/FBQueryParameters' );
+  var MysteryScreen = require( 'FUNCTION_BUILDER/mystery/MysteryScreen' );
+  var PatternsScreen = require( 'FUNCTION_BUILDER/patterns/PatternsScreen' );
+  var Sim = require( 'JOIST/Sim' );
+  var SimLauncher = require( 'JOIST/SimLauncher' );
+  var TestScreen = require( 'FUNCTION_BUILDER/test/TestScreen' );
+  var Tandem = require( 'TANDEM/Tandem' );
+
+  // strings
+  var functionBuilderTitleString = require( 'string!FUNCTION_BUILDER/function-builder.title' );
+
+  // constants
+  var tandem = Tandem.createRootTandem();
+
+  var options = {
+    credits: {
+      leadDesign: 'Amanda McGarry',
+      softwareDevelopment: 'Chris Malley (PixelZoom, Inc.)',
+      team: 'Amy Hanson, Karina K. R. Hensberry, Ariel Paul, Kathy Perkins,\nSam Reid, Beth Stade, David Webb',
+      qualityAssurance: 'Steele Dalton, Amanda Davis, Andrea Lin, Ben Roberts'
+    }
+  };
+
+  SimLauncher.launch( function() {
+
+    var screens = [
+      new PatternsScreen( tandem.createTandem( 'patternsScreen' ) ),
+      new MysteryScreen( tandem.createTandem( 'mysteryScreen' ) )
+    ];
+
+    if ( FBQueryParameters.testScreen ) {
+      screens.push( new TestScreen() );
+    }
+
+    var sim = new Sim( functionBuilderTitleString, screens, options );
+    sim.start();
+  } );
+} );
