fd72edfff810d73343ccc6e07914fe3f5796e7d1
Chris Malley <cmalley@pixelzoom.com>
2017-03-12 17:51:54 -0600

    SIM_NAME placeholder #4

--- js/function-basics-main.js
+++ js/function-basics-main.js
@@ -1,7 +1,7 @@
 // Copyright 2015-2016, University of Colorado Boulder
 
 /**
- * Main entry point for the 'Function Basics' sim.
+ * Main entry point for the 'SIM_NAME' sim.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
