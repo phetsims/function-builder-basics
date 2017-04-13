beed40f796986a82c0245490552eef6dbba4a023
Chris Malley <cmalley@pixelzoom.com>
2017-04-13 13:56:11 -0600

    revert color change to Patterns screen, https://github.com/phetsims/function-basics/issues/12

--- js/function-basics-main.js
+++ js/function-basics-main.js
@@ -9,11 +9,9 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var FBIconFactory = require( 'FUNCTION_BUILDER/common/view/FBIconFactory' );
   var FBQueryParameters = require( 'FUNCTION_BUILDER/common/FBQueryParameters' );
   var MysteryScreen = require( 'FUNCTION_BASICS/mystery/MysteryScreen' );
   var PatternsScreen = require( 'FUNCTION_BUILDER/patterns/PatternsScreen' );
-  var Property = require( 'AXON/Property' );
   var Sim = require( 'JOIST/Sim' );
   var SimLauncher = require( 'JOIST/SimLauncher' );
   var TestScreen = require( 'FUNCTION_BUILDER/test/TestScreen' );
@@ -24,7 +22,6 @@ define( function( require ) {
 
   // constants
   var tandem = Tandem.createRootTandem();
-  var SCREEN_COLOR = '#E1F7FF'; // see issue #12
 
   var options = {
     credits: {
@@ -38,10 +35,7 @@ define( function( require ) {
   SimLauncher.launch( function() {
 
     var screens = [
-      new PatternsScreen( tandem.createTandem( 'patternsScreen' ), {
-        backgroundColorProperty: new Property( SCREEN_COLOR ),
-        homeScreenIcon: FBIconFactory.createPatternsScreenIcon( { fill: SCREEN_COLOR } )
-      } ),
+      new PatternsScreen( tandem.createTandem( 'patternsScreen' ) ),
       new MysteryScreen( tandem.createTandem( 'mysteryScreen' ) )
     ];
 
