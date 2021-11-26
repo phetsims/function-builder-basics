7f0d3157d72760545a8555be086e423efff4603b
Jonathan Olson <jonathan.olson@colorado.edu>
2021-11-25 19:19:48 -0700

    Initial common code typescript support, including Scenery import changes to prevent out-of-order load errors

--- js/mystery/view/FBBMysteryFunctionNode.js
+++ js/mystery/view/FBBMysteryFunctionNode.js
@@ -10,7 +10,7 @@ import FBConstants from '../../../../function-builder/js/common/FBConstants.js';
 import ImageFunctionNode from '../../../../function-builder/js/common/view/functions/ImageFunctionNode.js';
 import functionBuilderStrings from '../../../../function-builder/js/functionBuilderStrings.js';
 import merge from '../../../../phet-core/js/merge.js';
-import Text from '../../../../scenery/js/nodes/Text.js';
+import { Text } from '../../../../scenery/js/imports.js';
 import functionBuilderBasics from '../../functionBuilderBasics.js';
 
 class FBBMysteryFunctionNode extends ImageFunctionNode {
