57c198f6c42975dfc67b7b1e7c4bf223f3b2f635
phet-dev <phetadmin@gmail.com>
2022-09-08 04:29:49 -0600

    update copyright dates from daily grunt work

--- js/mystery/view/FBBMysteryFunctionNode.js
+++ js/mystery/view/FBBMysteryFunctionNode.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2021, University of Colorado Boulder
+// Copyright 2017-2022, University of Colorado Boulder
 
 /**
  * Function for the 'Mystery' screen in 'Function Builder: Basics'.
